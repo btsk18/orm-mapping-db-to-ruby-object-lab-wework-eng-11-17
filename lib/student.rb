fa1e9b634ae58324fd295c85cc6b2323757f256b
Brandon Leeds <brandonleeds@gmail.com>
2018-01-16 22:08:49 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -77,4 +77,11 @@ class Student
     SQL
     DB[:conn].execute(first_query, num).map { |row| new_from_db(row) }
   end
+
+  def self.first_student_in_grade_10
+    first_query = <<-SQL
+    SELECT * FROM students WHERE grade = 10 LIMIT 1
+    SQL
+    DB[:conn].execute(first_query, num).map { |row| new_from_db(row) }
+  end
 end
